f35a930d6a35e520bb03687bb8d9323225f14dde
llouice <g13920228592@gmail.com>
2020-12-11 15:59:07 +0800

    fix release file

--- pkg/druid_web_examples.d.ts
+++ pkg/druid_web_examples.d.ts
@@ -87,3 +87,56 @@ export function widget_gallery(): void;
 /**
 */
 export function text(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly anim: () => void;
+  readonly calc: () => void;
+  readonly cursor: () => void;
+  readonly custom_widget: () => void;
+  readonly either: () => void;
+  readonly event_viewer: () => void;
+  readonly flex: () => void;
+  readonly game_of_life: () => void;
+  readonly hello: () => void;
+  readonly identity: () => void;
+  readonly image: () => void;
+  readonly invalidation: () => void;
+  readonly layout: () => void;
+  readonly lens: () => void;
+  readonly list: () => void;
+  readonly multiwin: () => void;
+  readonly open_save: () => void;
+  readonly panels: () => void;
+  readonly parse: () => void;
+  readonly scroll_colors: () => void;
+  readonly scroll: () => void;
+  readonly split_demo: () => void;
+  readonly styled_text: () => void;
+  readonly switches: () => void;
+  readonly timer: () => void;
+  readonly tabs: () => void;
+  readonly view_switcher: () => void;
+  readonly widget_gallery: () => void;
+  readonly text: () => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0cda70b23dc60b63: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h84af7ed0c10de93e: (a: number, b: number) => void;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+        
